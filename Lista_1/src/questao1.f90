5c7e74eee728552356cadbba69b482b89b90300b
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-07-22 12:35:49 -0300

    .gitignore e questões 1 e 2 refatoradas
    Acrescentou arquivo .gitignore para os produtos da compilação do
    Fortran. E adicionou nova versão comentada das questões 1 e 2.

--- Lista_1/src/questao1.f90
+++ Lista_1/src/questao1.f90
@@ -1,9 +1,39 @@
 PROGRAM sample_1
+!
+! Objetivo: (Questão 1 - lista 1) Mostrar as operações entre
+! os tipos primitivos reais e inteiro.
+!
+! Versão 1.0
+!
+! Programador: Rodolfo A. C. Neves 05/04/2018
+!
+! Email: rodolfo_profissional@hotmail.com
+!
+! Site: http://www.dirackslounge.online
+
+IMPLICIT NONE ! Não permitir variável implícita
+
 INTEGER :: i1, i2, i3
 REAL :: a1=2.4, a2
-i1 = a1
-i2 = INT(a1*i1)
+
+! i1=2
+i1 = a1 ! a1 é REAL, porém i1 é INTEGER, 
+! logo i1 recebe apenas a parte inteira de a1, se a1=2.4 i1=2 (parte inteira apenas)
+
+!i2 = 4
+i2 = INT(a1*i1) ! a multiplicação é feita nos reais, porém a função INT(x) 
+! permite apenas a atribuição da parte inteira
+! à i2
+
+! i3=5
+! NINT(x) arredonda para o inteiro mais próximo se a1=2.4 e i1=2, a1*i1 é arredondado para
+! 5 
 i3 = NINT(a1*i1)
-a2 = a1**i1
+
+! o expoente i1 é inteiro porém a base a1 é real e a conta é feita nos reais e atribuída
+! a a2, também real. 
+a2 = a1**i1 !2.4^2=2.4*2.4=5.76
+
+! Escreva os valores na tela
 WRITE(*,*) i1, i2, i3, a1, a2
-END PROGRAM sample_1
+END PROGRAM 
--- Lista_1/src/questao2.f90
+++ Lista_1/src/questao2.f90
@@ -1,10 +1,30 @@
-PROGRAM sample_1
-REAL :: a, b
-INTEGER :: n, i, j
-i = 10.
-j = 3
-n = i / j
-a = i / j
-b = REAL(i) / j
-write(*,*) a,b,n
-END PROGRAM sample_1
+PROGRAM questao2
+!
+! Objetivo: Resolução questão 2 da Lista 1, exemplificar
+! operações com tipos primitivos reais e inteiros.
+!
+! Versão 1.0
+!
+! Programador: Rodolfo A. C. Neves 23/04/2018
+
+IMPLICIT NONE ! Não permitir variável implícita
+
+! Declaração de Variáveis
+REAL:: a, b ! 2.4 5.3 3.3 0.125
+INTEGER:: n, i, j ! 1 2 3 -1 0 100
+
+i=10. ! i é inteiro, apesar do ponto i=10
+j=3 ! j é inteiro
+n=i/j ! 10/3=3  a divisão é feita nos inteiros, e é atribuída a um número inteiro n
+a=i/j ! 10/3=3 a divisão é feita nos inteiros, e é atribuída a um número real a
+
+b=REAL(i)/j ! 10/3=3.333333 a divisão é feita nos reais, pois i é convertido 
+
+!Exibindo as variáveis na tela
+WRITE(*,*) "i=", i
+WRITE(*,*) "j=", j
+WRITE(*,*) "n=", n
+WRITE(*,*) "a=", a
+WRITE(*,*) "b=", b
+
+END PROGRAM
