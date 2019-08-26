c94ddf2c5bac10c2f4aa2307288848fa91eac2ee
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-08-26 19:37:14 -0300

    resolução da questão 6

--- Lista_1/src/questao6.f90
+++ Lista_1/src/questao6.f90
@@ -1,14 +1,29 @@
-program distancia
+PROGRAM distancia
+!
+! questao6.f90 (Fortran)
+! 
+! Objetivo: Resolução da questão 6 da lista 1 de Fortran.
+! Programa para calcular a distância entre dois pontos.
+! 
+! Versão 1.0
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Programador: Rodolfo A. C. Neves (Dirack) 26/08/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: Software de uso livre e código aberto.
+IMPLICIT NONE
 
-implicit none
+REAL:: x1,y1
+REAL:: x2,y2
 
-real :: x1,y1,x2,y2 ! Coordenadas e distância
+WRITE(*,*) "Entre com o ponto 1  (x1,y1):"
+READ(*,*) x1,y1
+WRITE(*,*) "Entre com o ponto 2 (x2,y2):"
+READ(*,*) x2,y2
 
-write(*,*) "Entre com o ponto 1  (x1,y1):"
-read(*,*) x1,y1
-write(*,*) "Entre com o ponto 2 (x2,y2):"
-read(*,*) x2,y2
+WRITE(*,*) "A distância entre esses pontos é:",sqrt((x2-x1)**2+(y2-y1)**2)
 
-write(*,*) "A distância entre esses pontos é:",sqrt((x2-x1)**2+(y2-y1)**2)
-
-end program distancia
+END PROGRAM distancia
